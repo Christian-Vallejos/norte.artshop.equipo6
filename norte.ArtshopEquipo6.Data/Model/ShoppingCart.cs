7d87971c27f859b9cf28c8615bad62f6f4bde0a1
Sincovich, Maximiliano <maximilianosincovich@hotmail.com>
2020-06-28 20:11:21 -0300

    Base Carrito

---
+++ norte.ArtshopEquipo6.Data/Model/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace norte.ArtshopEquipo6.Data.Model
+{
+    public class ShoppingCart
+    {
+        public List<Producto> Productos { get; set; }
+
+
+        public double ValorFinal { get {
+                return this.Productos.Select(x => x.Price).ToList().Sum();
+                    } }
+
+
+
+    }
+}
