9da0004468b8c6302dcbf843b00d653249d7cbc4
Jorge Rodriguez <jorge-11@live.com.ar>
2020-06-28 17:26:22 -0300

    se agrego estructura de conexion bd y artista

--- norte.ArtshopEquipo6.Data/Model/Artista.cs
+++ norte.ArtshopEquipo6.Data/Model/Artista.cs
@@ -1,28 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace norte.ArtshopEquipo6.Data.Model
 {
 
-	public class Artista
+    //public class Artista
+    //   {
+    //	public int Id { get; set; }
+    //	public string FirstName { get; set; }
+
+    //	public string LastName { get; set; }
+
+    //	public string LifeSpan { get; set; }
+
+    //	public string Country { get; set; }
+
+    //	public string Description { get; set; }
+    //	public int TotalProducts { get; set; }
+    //	public DateTime CreatedOn { get; set; }
+    //	public string CreatedBy { get; set; }		
+    //	public DateTime ChangedOn { get; set; }
+    //	public string ChangedBy { get; set; }
+    //	}
+
+    public class Artista : IdentityBase
     {
-		public int Id { get; set; }
-		public string FirstName { get; set; }
+        public Artista()
+        {
+            this.Products = new HashSet<Producto>();
+        }
 
-		public string LastName { get; set; }
+        [Required]
+        [DisplayName("Nombre")]
+        public string FirstName { get; set; }
 
-		public string LifeSpan { get; set; }
+        [Required]
+        [DisplayName("Apellido")]
+        public string LastName { get; set; }
 
-		public string Country { get; set; }
+        public string LifeSpan { get; set; }
 
-		public string Description { get; set; }
-		public int TotalProducts { get; set; }
-		public DateTime CreatedOn { get; set; }
-		public string CreatedBy { get; set; }		
-		public DateTime ChangedOn { get; set; }
-		public string ChangedBy { get; set; }
-		}
+        [Required]
+        [DisplayName("País")]
+        public string Country { get; set; }
+
+        [DisplayName("Descripción")]
+        public string Description { get; set; }
+
+        [Required]
+        [DisplayName("Recetas")]
+        public int TotalProducts { get; set; }
+
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
+        public virtual ICollection<Producto> Products { get; set; }
+
+    }
 }
---
+++ norte.ArtshopEquipo6.Data/Services/ArtShopDbContext.cs
@@ -0,0 +1,35 @@
+﻿using norte.ArtshopEquipo6.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace norte.ArtshopEquipo6.Data.Services
+{
+    public partial class ArtShopDbContext : DbContext
+    {
+        public ArtShopDbContext() : base("name=DefaultConnection")
+        {
+            Database.SetInitializer<ArtShopDbContext>(null);
+        }
+        /// <summary>
+        /// PluralizingTableNameConvention
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+        /// DbSet Artist se utiliza para representar una tabla.
+        public virtual DbSet<Artista> Artist { get; set; }
+
+        /// DbSet Product se utiliza para representar una tabla.
+        public virtual DbSet<Producto> Product { get; set; }
+        /// DbSet Product se utiliza para representar una tabla.
+        public virtual DbSet<Error> Error { get; set; }
+
+    }
+}
