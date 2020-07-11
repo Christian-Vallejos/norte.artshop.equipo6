173e4fbb22476533bbcc5133af2ee5fa57ff54f5
Sincovich, Maximiliano <maximilianosincovich@hotmail.com>
2020-07-11 15:29:51 -0300

    post

--- norteArtshopEquipo6.WebSite/Controllers/ProductoController.cs
+++ norteArtshopEquipo6.WebSite/Controllers/ProductoController.cs
@@ -10,6 +10,7 @@ using System.Net;
 
 namespace norteArtshopEquipo6.WebSite.Controllers
 {
+    [Authorize(Roles ="Management")]
     public class ProductoController : BaseControllerBorrar
     {
         private BaseDataService<Product> db = new BaseDataService<Product>();
